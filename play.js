52241f39695844ba4920323ab50fefadb0b753da
Vaishali Gupta <grvaishali@gmail.com>
2020-08-26 02:48:04 +0000

    Handle key

--- play.js
+++ play.js
@@ -1,3 +1,26 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on("data", function (data) {
+    if (data === '\u0003') {
+      process.exit();
+    }
+  });
+  stdin.resume();
+  return stdin;
+}
+
+const handleUserInput = function () {
+
+}
+
+setupInput();
