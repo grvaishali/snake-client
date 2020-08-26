61ad4325699aafba8291c3263365d4ad9e4396ae
Vaishali Gupta <grvaishali@gmail.com>
2020-08-26 02:54:25 +0000

    Added input.js

---
+++ input.js
@@ -0,0 +1,20 @@
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+}
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.on("data", handleUserInput)
+  stdin.resume();
+  return stdin;
+}
+
+module.exports = { setupInput }
--- play.js
+++ play.js
@@ -1,26 +1,7 @@
-const connect = require('./client');
+const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
-
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.on("data", function (data) {
-    if (data === '\u0003') {
-      process.exit();
-    }
-  });
-  stdin.resume();
-  return stdin;
-}
-
-const handleUserInput = function () {
-
-}
 
 setupInput();
